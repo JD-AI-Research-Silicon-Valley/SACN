9517c10f15f6ca740c7215263731299cff070894
Chao Shang <chaoshang@Joe.local>
2019-02-17 15:05:23 -0500

    Update

--- run_main.sh
+++ run_main.sh
@@ -2,16 +2,11 @@
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-TRAIN_PATH=$SCRIPT_DIR"/main_1.py"
-LOG="logs/main_1-fb"
+TRAIN_PATH=$SCRIPT_DIR"/main.py"
+LOG="logs/main"
 
-source activate chao_env
+source activate ENV_NAME
 
-/mnt/cephfs2/asr/users/chao.shang/script/queue/queue.pl -q g.q -l gpu=1 $LOG bash /mnt/cephfs2/asr/users/chao.shang/script/run_gpu.sh /opt/cephfs1/asr/users/chao.shang/anaconda3/envs/chao_env/bin/python -u $TRAIN_PATH model ConvE dataset FB15k-237 process True &
+/.../script/queue/queue.pl -q g.q -l gpu=1 $LOG bash /.../script/run_gpu.sh /.../anaconda3/envs/ENV_NAME/bin/python -u $TRAIN_PATH model ConvE dataset FB15k-237 process True &
 
 source  deactivate
-
-# lr 0.003
-# dropout 0.2
-# nn.Conv1d(2, 100)
-# init_emb_size = 200
